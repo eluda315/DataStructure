2a54e2f240d1a4cd06e9c040b859f650bc03fa68
eluda315 <michelle3hg@gmail.com>
2025-08-06 14:33:36 +0900

    feat: CircularQueue 시작

---
+++ 3장 큐/CircularQueue/CircularQueue/CircularQueue.c
@@ -0,0 +1,42 @@
+#include "CircularQueue.h"
+
+void CQ_CreateQueue(CircularQueue** Queue, int Capacity)
+{
+
+}
+
+
+void CD_DestroyQueue(CircularQueue* Queue)
+{
+
+}
+
+
+void CQ_Enqueue(CircularQueue* Queue, ElementType Data)
+{
+
+}
+
+
+ElementType CQ_Dequeue(CircularQueue* Queue)
+{
+
+}
+
+
+int CQ_GetSize(CircularQueue* Queue)
+{
+
+}
+
+
+int CQ_IsEmpty(CircularQueue* Queue)
+{
+
+}
+
+
+int DQ_IsFull(CircularQueue* Queue)
+{
+
+}
