646db608ad5e0b3bbe2553c52a1f898029711ac3
eluda315 <michelle3hg@gmail.com>
2025-08-07 20:11:07 +0900

    feat: completely implement CircularQueue

--- 3장 큐/CircularQueue/CircularQueue/CircularQueue.c
+++ 3장 큐/CircularQueue/CircularQueue/CircularQueue.c
@@ -2,41 +2,72 @@
 
 void CQ_CreateQueue(CircularQueue** Queue, int Capacity)
 {
+	*Queue = (CircularQueue*)malloc(sizeof(CircularQueue));
 
+	(*Queue)->Capacity = Capacity;
+	(*Queue)->Front = 0;
+	(*Queue)->Rear = 0;
+
+	(*Queue)->Nodes = (Node*)malloc(sizeof(Node) * (Capacity + 1));
 }
 
 
-void CD_DestroyQueue(CircularQueue* Queue)
+void CQ_DestroyQueue(CircularQueue* Queue)
 {
-
+	free(Queue->Nodes);
+	free(Queue);
 }
 
 
 void CQ_Enqueue(CircularQueue* Queue, ElementType Data)
 {
-
+	if (CQ_IsFull(Queue)) // ²Ë Ã¡À¸¸é
+	{
+		printf("! Queue is Full !\n");
+	}
+	else // °ø°£ ÀÖÀ¸¸é
+	{
+		Queue->Nodes[Queue->Rear].Data = Data;
+		if (Queue->Rear == Queue->Capacity) Queue->Rear = 0;
+		else Queue->Rear++;
+	}
 }
 
 
 ElementType CQ_Dequeue(CircularQueue* Queue)
 {
+	int position = 0;
 
+	if (CQ_IsEmpty(Queue)) printf("! Queue is empty !\n");
+	else
+	{
+		position = Queue->Front;
+		if (Queue->Front == Queue->Capacity) Queue->Front = 0;
+		else Queue->Front++;
+	}
+
+	return Queue->Nodes[position].Data;
 }
 
 
 int CQ_GetSize(CircularQueue* Queue)
 {
+	int size = 0;
+	if (Queue->Front <= Queue->Rear) size = Queue->Rear - Queue->Front;
+	else size = Queue->Capacity + Queue->Rear - Queue->Front + 1;
 
+	return size;
 }
 
 
 int CQ_IsEmpty(CircularQueue* Queue)
 {
-
+	return (Queue->Front == Queue->Rear);
 }
 
 
-int DQ_IsFull(CircularQueue* Queue)
+int CQ_IsFull(CircularQueue* Queue)
 {
-
+	if (Queue->Front < Queue->Rear) return (Queue->Rear - Queue->Front) == Queue->Capacity;
+	else return (Queue->Front == (Queue->Rear + 1));
 }
--- 3장 큐/CircularQueue/CircularQueue/CircularQueue.h
+++ 3장 큐/CircularQueue/CircularQueue/CircularQueue.h
@@ -21,11 +21,11 @@ typedef struct TagCircularQueue
 }CircularQueue;
 
 void CQ_CreateQueue(CircularQueue** Queue, int Capacity);
-void CD_DestroyQueue(CircularQueue* Queue);
+void CQ_DestroyQueue(CircularQueue* Queue);
 void CQ_Enqueue(CircularQueue* Queue, ElementType Data);
 ElementType CQ_Dequeue(CircularQueue* Queue);
 int CQ_GetSize(CircularQueue* Queue);
 int CQ_IsEmpty(CircularQueue* Queue);
-int DQ_IsFull(CircularQueue* Queue);
+int CQ_IsFull(CircularQueue* Queue);
 
 #endif
