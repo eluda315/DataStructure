485ac1bf7efaf59fbbb37c32022bdd5d660f6a09
eluda315 <michelle3hg@gmail.com>
2025-08-09 10:58:08 +0900

    feat: finish LCRSTree

--- 4장 트리/LCRSTree/LCRSTree.h
+++ 4장 트리/LCRSTree/LCRSTree.h
@@ -6,18 +6,20 @@
 
 typedef char ElementType;
 
-typedef struct TagLCRSNode
+typedef struct tagLCRSNode
 {
-	struct TagLCRSNode* LeftChild;
-	struct TagLCRSNode* RightSibling;
-	ElementType Data;
+    struct tagLCRSNode* LeftChild;
+    struct tagLCRSNode* RightSibling;
+
+    ElementType Data;
 } LCRSNode;
 
-LCRSNode* LCRS_CreateNode(ElementType NewData);
-void LCRS_DestroyNode(LCRSNode* Node);
-void LCRS_DestroyTree(LCRSNode* Root);
 
-void LCRS_AddChildNode(LCRSNode* ParentNode, LCRSNode* ChildNode);
-void LCRS_PrintTree(LCRSNode* Node, int Depth);
+LCRSNode* LCRS_CreateNode(ElementType NewData);
+void      LCRS_DestroyNode(LCRSNode* Node);
+void      LCRS_DestroyTree(LCRSNode* Root);
+
+void      LCRS_AddChildNode(LCRSNode* ParentNode, LCRSNode* ChildNode);
+void      LCRS_PrintTree(LCRSNode* Node, int Depth);
 
 #endif
--- 4장 트리/LCRSTree/Test_LCRSTree.c
+++ 4장 트리/LCRSTree/Test_LCRSTree.c
@@ -1,26 +1,40 @@
 #include "LCRSTree.h"
 
-LCRSNode* LCRS_CreateNode(ElementType NewData)
+int main(void)
 {
+    //  노드 생성 
+    LCRSNode* Root = LCRS_CreateNode('A');
 
-}
+    LCRSNode* B = LCRS_CreateNode('B');
+    LCRSNode* C = LCRS_CreateNode('C');
+    LCRSNode* D = LCRS_CreateNode('D');
+    LCRSNode* E = LCRS_CreateNode('E');
+    LCRSNode* F = LCRS_CreateNode('F');
+    LCRSNode* G = LCRS_CreateNode('G');
+    LCRSNode* H = LCRS_CreateNode('H');
+    LCRSNode* I = LCRS_CreateNode('I');
+    LCRSNode* J = LCRS_CreateNode('J');
+    LCRSNode* K = LCRS_CreateNode('K');
 
-void LCRS_DestroyNode(LCRSNode* Node)
-{
+    //  트리에 노드 추가 
+    LCRS_AddChildNode(Root, B);
+    LCRS_AddChildNode(B, C);
+    LCRS_AddChildNode(B, D);
+    LCRS_AddChildNode(D, E);
+    LCRS_AddChildNode(D, F);
 
-}
+    LCRS_AddChildNode(Root, G);
+    LCRS_AddChildNode(G, H);
 
-void LCRS_DestroyTree(LCRSNode* Root)
-{
+    LCRS_AddChildNode(Root, I);
+    LCRS_AddChildNode(I, J);
+    LCRS_AddChildNode(J, K);
 
-}
+    //  트리 출력 
+    LCRS_PrintTree(Root, 0);
 
-void LCRS_AddChildNode(LCRSNode* ParentNode, LCRSNode* ChildNode)
-{
+    //  트리 소멸시키기 
+    LCRS_DestroyTree(Root);
 
-}
-
-void LCRS_PrintTree(LCRSNode* Node, int Depth)
-{
-
+    return 0;
 }
